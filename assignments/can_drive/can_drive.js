06a8ee34085e8f98423517805fd3e1731c0492b0
hridesh121 <hridesh.kushwaha15@gmail.com>
2022-01-18 11:38:27 +0530

    JavaScript_Assignment-2 Submission

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,17 @@
+
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if (hasDrivingLiscence!==true){
+        return "You cannot drive"
+    }
+    else if(hasDrivingLiscence===true && isTired!==true && isSober===true){
+        return "You can drive"
+    }
+    else {
+        return "You shouldn't drive"
+    }
+    
+
 }
 
 module.exports = CanDrive;
