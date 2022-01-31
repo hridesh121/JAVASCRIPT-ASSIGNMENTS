6f183a6c27049cc1d1cbbe29400193b4955f831b
hridesh121 <hridesh.kushwaha15@gmail.com>
2022-01-31 11:32:22 +0530

    todo_list assignment submission

---
+++ assignments/todo_list/script.js
@@ -0,0 +1,21 @@
+var todobtn = document.querySelector('button');
+var inputfld = document.querySelector('input');
+var todo = document.querySelector('.todos')
+var i = 0;
+
+
+todobtn.addEventListener("click", additem)
+
+function additem(){
+    var paragraph = document.createElement('p')
+    paragraph.setAttribute("key",i);
+    i = i + 1;
+    paragraph.innerHTML = inputfld.value;
+    todo.appendChild(paragraph);
+    inputfld.value="";
+    paragraph.addEventListener("click", deleteitem)
+
+    function deleteitem(){
+        todo.removeChild(paragraph)
+    }
+}
